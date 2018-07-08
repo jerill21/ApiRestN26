beca5660d10ce795747cdb2fac77fd2c1603b86b
jerill21 <jerillroig@gmail.com>
2018-07-08 20:17:31 +0200

    API REST N26 transaction service

--- src/test/java/com/transactions/statistics/TransactionControllerTest.java
+++ src/test/java/com/transactions/statistics/TransactionControllerTest.java
@@ -7,8 +7,10 @@ import static org.junit.Assert.assertTrue;
 import java.text.ParseException;
 import java.util.List;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,6 +19,7 @@ import com.transactions.statistics.controller.TransactionController;
 import com.transactions.statistics.dto.TransactionDTO;
 
 @RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SpringBootTest
 public class TransactionControllerTest {
 
@@ -24,7 +27,7 @@ public class TransactionControllerTest {
 	TransactionController controller;
 	
 	@Test
-	public void test_createTransaction201_accept() throws ParseException {
+	public void test01_createTransaction201_accept() throws ParseException {
 		
 		TransactionDTO transaction = new TransactionDTO();
 		long currentTimestamp = System.currentTimeMillis();
@@ -36,7 +39,7 @@ public class TransactionControllerTest {
 	}
 
 	@Test
-	public void test_createTransaction204_accept() throws ParseException {
+	public void test02_createTransaction204_accept() throws ParseException {
 		
 		TransactionDTO transaction = new TransactionDTO();
 		long currentTimestamp = System.currentTimeMillis() - 70000;
@@ -48,7 +51,7 @@ public class TransactionControllerTest {
 	}
 	
 	@Test
-	public void test_findAll_accept() {
+	public void test03_findAll_accept() {
 		
 		List<TransactionDTO> transactionList = controller.findAllTransactions();
 		assertNotNull(transactionList);
